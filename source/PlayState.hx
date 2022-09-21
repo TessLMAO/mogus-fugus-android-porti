c723d644ccc380f1e2724d21e2787da350de9362
TessLMAO <110305665+TessLMAO@users.noreply.github.com>
2022-09-21 15:52:14 -0300

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1105,7 +1105,7 @@ class PlayState extends MusicBeatState
 		add(botplayTxt);
 
 		creditTxt = new FlxText(876, 648, 348);
-    creditTxt.text = "PORTED BY/nTESS";
+    creditTxt.text = "PORTED BY TESS";
     creditTxt.setFormat(Paths.font("vcr.ttf"), 30, FlxColor.WHITE, RIGHT, FlxTextBorderStyle.OUTLINE,FlxColor.BLACK);
     creditTxt.scrollFactor.set();
     add(creditTxt);
